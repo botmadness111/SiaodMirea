50b1c44f71becc2e20f8d23978033ec5f2b16f66
qqAndrey <63346175+botmadness111@users.noreply.github.com>
2023-09-24 15:11:19 +0300

    dfg

---
+++ 1/Task2.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Andre on 24.09.2023.
+//
+
+#include <iostream>
+#include <fstream>
+#include <ostream>
+#include <random>
+#include <sstream>
+
+using namespace std;
+
+int main() {
+
+
+    return 0;
+}
