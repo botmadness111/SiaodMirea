b3aec5aa2b1d9cacbd61920b2e48fc777bcfd305
qqAndrey <63346175+botmadness111@users.noreply.github.com>
2023-10-08 21:35:32 +0300

    comp 3prac 2 task

---
+++ 1/3/Hash.h
@@ -0,0 +1,14 @@
+//
+// Created by Andre on 08.10.2023.
+//
+
+#ifndef SIAODMIREA_HASH_H
+#define SIAODMIREA_HASH_H
+
+
+class Hash {
+
+};
+
+
+#endif //SIAODMIREA_HASH_H
---
+++ 1/3/HashTable.h
@@ -0,0 +1,14 @@
+//
+// Created by Andre on 08.10.2023.
+//
+
+#ifndef SIAODMIREA_HASHTABLE_H
+#define SIAODMIREA_HASHTABLE_H
+
+
+class HashTable {
+
+};
+
+
+#endif //SIAODMIREA_HASHTABLE_H
